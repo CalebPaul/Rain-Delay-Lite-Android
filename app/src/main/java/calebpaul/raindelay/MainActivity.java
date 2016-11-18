0cfb62d797b362e89d733d28bddb070192e05277
Caleb Paul <calebpaul21@gmail.com>
2016-11-18 10:25:34 -0800

    add intent on button click to go from main to set conditions activity

--- app/src/main/java/calebpaul/raindelay/MainActivity.java
+++ app/src/main/java/calebpaul/raindelay/MainActivity.java
@@ -1,13 +1,33 @@
 package calebpaul.raindelay;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.setWeatherButton) Button mSetConditionsButton;
+    @Bind(R.id.viewConditionsButton) Button mViewConditionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSetConditionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SetConditionsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
