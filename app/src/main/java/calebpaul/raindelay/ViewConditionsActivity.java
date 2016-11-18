8c90667279d39d2ba2f92d84b12d9464d4a1f669
Caleb Paul <calebpaul21@gmail.com>
2016-11-18 11:34:22 -0800

    add and bind textviews for ViewConditions activity

--- app/src/main/java/calebpaul/raindelay/ViewConditionsActivity.java
+++ app/src/main/java/calebpaul/raindelay/ViewConditionsActivity.java
@@ -2,12 +2,21 @@ package calebpaul.raindelay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ViewConditionsActivity extends AppCompatActivity {
+    @Bind(R.id.displayConditionName) TextView mConditionNameView;
+    @Bind(R.id.maxWindTextView) TextView mWindView;
+    @Bind(R.id.minTempTextView) TextView mMinTempView;
+    @Bind(R.id.maxTempTextView) TextView mMaxTempView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_conditions);
+        ButterKnife.bind(this);
     }
 }
