e7dbfa2bcbe575c4f57b31aa4dca3d203e148d6e
Caleb Paul <calebpaul21@gmail.com>
2016-11-21 10:31:01 -0800

    add material design colors and style buttons

--- app/src/main/java/calebpaul/raindelay/MainActivity.java
+++ app/src/main/java/calebpaul/raindelay/MainActivity.java
@@ -1,16 +1,19 @@
 package calebpaul.raindelay;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.mainTitleText) TextView mTitleText;
     @Bind(R.id.setWeatherButton) Button mConditionsButton;
     @Bind(R.id.viewForecastButton) Button mViewForecastButton;
 
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Typeface Voltaire = Typeface.createFromAsset(getAssets(), "fonts/Voltaire-Regular.otf");
+        mTitleText.setTypeface(Voltaire);
 
         mConditionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
