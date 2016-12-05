4ca0deba08d6c4f77b8fa1ea7459ecfb780b92f9
Caleb Paul <calebpaul21@gmail.com>
2016-12-04 20:53:39 -0800

    add and populate methods of ForecastListAdapter

---
+++ app/src/main/java/calebpaul/raindelay/adapters/ForecastListAdapter.java
@@ -0,0 +1,65 @@
+package calebpaul.raindelay.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import calebpaul.raindelay.R;
+import calebpaul.raindelay.models.Forecast;
+
+/**
+ * Created by calebpaul on 12/4/16.
+ */
+public class ForecastListAdapter extends RecyclerView.Adapter<ForecastListAdapter.ForecastViewHolder> {
+    private ArrayList<Forecast> mForecasts = new ArrayList<>();
+    private Context mContext;
+
+    public ForecastListAdapter(Context context, ArrayList<Forecast> forecasts) {
+        mContext = context;
+        mForecasts = forecasts;
+    }
+
+    @Override
+    public ForecastListAdapter.ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.forecast_list_item, parent, false);
+        ForecastViewHolder viewHolder = new ForecastViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastListAdapter.ForecastViewHolder holder, int position) {
+        holder.bindForecast(mForecasts.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ForecastViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.summaryTextView) TextView mSummaryTextView;
+        @Bind(R.id.windSpeedTextView) TextView mWindSpeedTextView;
+        @Bind(R.id.temperatureTextView) TextView mTemperatureTextView;
+
+        private Context mContext;
+
+        public ForecastViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindForecast(Forecast forecast) {
+            mSummaryTextView.setText(forecast.getSummary());
+            mWindSpeedTextView.setText(forecast.getWindSpeed());
+            mTemperatureTextView.setText(forecast.getTemperature());
+        }
+    }
+}
