3e6e9165eacd05236ba1b3a41a305aac2fe1adc4
Caleb Paul <calebpaul21@gmail.com>
2016-12-05 13:16:08 -0800

    add implicit intent on forecast click

--- app/src/main/java/calebpaul/raindelay/adapters/ForecastListAdapter.java
+++ app/src/main/java/calebpaul/raindelay/adapters/ForecastListAdapter.java
@@ -1,7 +1,10 @@
 package calebpaul.raindelay.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,7 +46,7 @@ public class ForecastListAdapter extends RecyclerView.Adapter<ForecastListAdapte
         return mForecasts.size();
     }
 
-    public class ForecastViewHolder extends RecyclerView.ViewHolder {
+    public class ForecastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         @Bind(R.id.summaryTextView) TextView mSummaryTextView;
         @Bind(R.id.temperatureTextView) TextView mTemperatureTextView;
         @Bind(R.id.windSpeedTextView) TextView mWindSpeedTextView;
@@ -59,6 +62,14 @@ public class ForecastListAdapter extends RecyclerView.Adapter<ForecastListAdapte
             mTemperatureTextView.setText("Temp approx " + String.valueOf(forecast.getTemperature()) + " Degrees");
             mWindSpeedTextView.setText("Winds up to " + String.valueOf(forecast.getWindSpeed()) + " MPH");
 
+            mSummaryTextView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.v("ForecastListActivity", "Clicky");
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://darksky.net"));
+            mContext.startActivity(webIntent);
         }
     }
 }
